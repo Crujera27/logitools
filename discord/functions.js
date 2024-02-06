80f7f4d65082cf5d8f39160fcd50633b3e721d6d
Ángel <71277595+Crujera27@users.noreply.github.com>
2024-02-06 17:10:12 +0100

    update

--- discord/functions.js
+++ discord/functions.js
@@ -1,39 +1,21 @@
-/*
-.____                 .__  __                .__          
-|    |    ____   ____ |__|/  |_  ____   ____ |  |   ______
-|    |   /  _ \ / ___\|  \   __\/  _ \ /  _ \|  |  /  ___/
-|    |__(  <_> ) /_/  >  ||  | (  <_> |  <_> )  |__\___ \ 
-|_______ \____/\___  /|__||__|  \____/ \____/|____/____  >
-        \/    /_____/                                  \/ 
-                         
-        
-    Copyright © 2024 Crujera27 y contribuidores. Todos los derechos reservados.
-    
-    GitHub: https://github.com/Crujera27/
-    Web: https://crujera.galnod.com
-    Licencia del proyecto: MIT
-
-*/
-
 /**
  * Logs a message with optional styling.
  *
  * @param {string} string - The message to log.
  * @param {'info' | 'err' | 'warn' | 'done' | undefined} style - The style of the log.
  */
-const log = (string, style) => {
-  if(style="info"){
-    console.warn('[INFO] ', string)
-  }else if(style="err"){
-    console.error('[ERROR] ', string)
-  }else if(style="warn"){
-    console.warn('[WARN] ', string)
-  }else if(style="done"){
-    console.log('[OK] ', string)
-  }else{
-    console.log(string)
-  }
-};
+let logPromise = (async () => {
+  const remotelogpromise = import('../tools/log.mjs');
+  const remotelog = await remotelogpromise;
+  return (string, style) => {
+    remotelog.default(string, style);
+  };
+})();
+
+(async () => {
+  const log = await logPromise;
+  
+})();
 
 /**
  * Formats a timestamp.
@@ -48,7 +30,7 @@ const time = (time, style) => {
 
 /**
  * Whenever a string is a valid snowflake (for Discord).
-
+ *
  * @param {string} id 
  * @returns {boolean}
  */
@@ -56,8 +38,13 @@ const isSnowflake = (id) => {
   return /^\d+$/.test(id);
 };
 
+
+
 module.exports = {
-  log,
+  log: async (string, style) => {
+    const log = await logPromise;
+    log(string, style);
+  },
   time,
   isSnowflake
 };

633830773cb4524add12ab28677203b785ac2730
Ángel <71277595+Crujera27@users.noreply.github.com>
2024-03-16 13:32:02 +0100

    Update

---
+++ database.sql
@@ -0,0 +1,83 @@
+-- --------------------------------------------------------
+-- SQL FOR LOGITOOLS BETA v.4.0.0 (16/03/20224)
+--
+-- Table structure for table `disciplinary_records`
+--
+
+CREATE TABLE `disciplinary_records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `offender_discord_id` int(255) NOT NULL,
+  `issuer_discord_id` int(255) NOT NULL,
+  `issue_date` date NOT NULL DEFAULT current_timestamp(),
+  `file_name` int(11) NOT NULL,
+  `file_internalname` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `punishment_history`
+--
+
+CREATE TABLE `punishment_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `discord_id` varchar(255) NOT NULL,
+  `punishment_type` enum('warn_mild','warn_middle','warn_severe','timeout','ban') NOT NULL,
+  `punishment_reason` text NOT NULL,
+  `issue_date` datetime NOT NULL,
+  `punishment_issuer` varchar(255) NOT NULL,
+  `expired` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `idx_discord_id` (`discord_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resources`
+--
+
+CREATE TABLE `resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `link` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `originalname` text CHARACTER SET utf16 COLLATE utf16_general_ci NOT NULL,
+  `createdby` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `user_id` int(11) NOT NULL,
+  `ticket_id` int(11) NOT NULL,
+  `ticket_status` varchar(20) NOT NULL,
+  `ticket_responses` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`ticket_responses`)),
+  PRIMARY KEY (`user_id`,`ticket_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `discord_id` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `discriminator` varchar(4) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `avatar_uuid` varchar(255) DEFAULT NULL,
+  `banned` tinyint(1) NOT NULL DEFAULT 0,
+  `isStaff` tinyint(1) NOT NULL DEFAULT 0,
+  `staffrank` text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT 0,
+  `adminVerifyStatus` enum('unverified','progress','locked','verified') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'unverified',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- routes/admin.mjs
+++ routes/admin.mjs
@@ -34,15 +34,161 @@ router.get('/admin/resources', isAuthenticated, isAdmin, async (req, res) => {
     return res.render('admin/resources', {resources: resources});
 })
 
+/* Staff Manager */
+
 router.get('/admin/staffmanager', isAuthenticated, isAdmin, async (req, res) => {
     const staffs = await executeQuery('SELECT * FROM users WHERE isStaff = 1');
     return res.render('admin/staffmanager', {staffs: staffs});
 })
 
+
+router.get('/admin/staffmanager/id/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const staff = await executeQuery('SELECT * FROM users WHERE isStaff = 1 AND discord_id = ?', [req.params.id]);
+        return res.render('admin/view-staff', { staff: staff[0] });
+    } catch (error) {
+        console.error('Error fetching staff members:', error);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/admin/staffmanager/setrank/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { staffRank } = req.body;
+        const updateQuery = 'UPDATE users SET staffrank = ? WHERE isStaff = 1 AND discord_id = ?';
+        await executeQuery(updateQuery, [staffRank, id]);
+        return res.redirect('/admin/staffmanager/id/' + id);
+    } catch (error) {
+        console.error('Error updating staff rank:', error);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.post('/admin/staffmanager/suspend/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuery = 'UPDATE users SET banned = 1 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [id]);
+        return res.redirect('/admin/staffmanager/id/' + id);
+    } catch (error) {
+        console.error('Error suspending user:', error);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.post('/admin/staffmanager/unsuspend/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuery = 'UPDATE users SET banned = 0 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [id]);
+        return res.redirect('/admin/staffmanager/id/' + id);
+    } catch (error) {
+        console.error('Error unsuspending user:', error);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.post('/admin/staffmanager/addadmin/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuery = 'UPDATE users SET isAdmin = 1 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [id]);
+        return res.redirect('/admin/staffmanager/id/' + id);
+    } catch (error) {
+        console.error('Error adding admin user:', error);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.post('/admin/staffmanager/removeadmin/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuery = 'UPDATE users SET isAdmin = 0 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [id]);
+        return res.redirect('/admin/staffmanager/id/' + id);
+    } catch (error) {
+        console.error('Error removing admin user:', error);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.post('/admin/staffmanager/removestaff/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuery = 'UPDATE users SET isStaff = 0, isAdmin = 0 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [id]);
+        return res.redirect('/admin/staffmanager');
+    } catch (error) {
+        console.error('Error removing staff member:', error);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+
+router.post('/admin/staffmanager/addstaff', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { discord_id } = req.body;
+        if (!discord_id) {
+            return res.redirect('/admin/staffmanager');
+        }
+        if (isNaN(discord_id)) {
+            return res.redirect('/admin/staffmanager');
+        }
+        const staffQuery = 'SELECT isStaff FROM users WHERE discord_id = ?';
+        const [existingStaff] = await executeQuery(staffQuery, [discord_id]);
+
+        if (existingStaff && existingStaff.isStaff === 1) {
+            return res.redirect('/admin/staffmanager');
+        }
+        const updateQuery = 'UPDATE users SET isStaff = 1 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [discord_id]);
+        return res.redirect('/admin/staffmanager');
+    } catch (error) {
+        console.error('Error updating user status:', error);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+
+
+
+/* End Staff Manager */
+
+/* User Manager */
+
 router.get('/admin/usermanager', isAuthenticated, isAdmin, async (req, res) => {
     const users = await executeQuery('SELECT * FROM users');
     return res.render('admin/usermanager', {users: users});
 })
+
+router.post('/admin/usermanager/suspend/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuery = 'UPDATE users SET banned = 1 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [id]);
+        return res.redirect('/admin/usermanager');
+    } catch (error) {
+        console.error('Error suspending user:', error);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.post('/admin/usermanager/unsuspend/:id', isAuthenticated, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updateQuery = 'UPDATE users SET banned = 0 WHERE discord_id = ?';
+        await executeQuery(updateQuery, [id]);
+        return res.redirect('/admin/usermanager');
+    } catch (error) {
+        console.error('Error unsuspending user:', error);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+/* End User Manager */
 
 router.post('/admin/addresource', isAuthenticated, isAdmin, upload.single('resourceFile'), async (req, res) => {
     const { resourceName } = req.body;

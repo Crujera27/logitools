f5c2106a25db1ec925037f879715e2a91a950d30
Ángel <71277595+Crujera27@users.noreply.github.com>
2025-11-26 22:29:36 +0100

    remove

---
+++ discord/events/Guild/messageCreate.js
@@ -0,0 +1,165 @@
+/*
+.____                 .__  __                .__          
+|    |    ____   ____ |__|/  |_  ____   ____ |  |   ______
+|    |   /  _ \ / ___\|  \   __\/  _ \ /  _ \|  |  /  ___/
+|    |__(  <_> ) /_/  >  ||  | (  <_> |  <_> )  |__\___ \ 
+|_______ \____/\___  /|__||__|  \____/ \____/|____/____  >
+        \/    /_____/                                  \/ 
+                         
+        
+    Copyright (C) 2024 Ángel Crujera (me@crujera.net)
+
+    This program is free software: you can redistribute it and/or modify  
+    it under the terms of the GNU Affero General Public License as published by  
+    the Free Software Foundation, either version 3 of the License, or  
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,  
+    but WITHOUT ANY WARRANTY; without even the implied warranty of  
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License  
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+    
+    GitHub: https://github.com/Crujera27/
+    Website: https://crujera.net
+
+*/
+
+const { ChannelType, Message } = require("discord.js");
+const config = require("../../config.js");
+const { log } = require("../../functions");
+const ExtendedClient = require("../../class/ExtendedClient.js");
+
+const cooldown = new Map();
+
+module.exports = {
+    event: "messageCreate",
+    /**
+     *
+     * @param {ExtendedClient} client
+     * @param {Message<true>} message
+     * @returns
+     */
+    run: async (client, message) => {
+        if (message.author.bot || message.channel.type === ChannelType.DM) return;
+
+        if (!config.handler.commands.prefix) return;
+
+        let prefix = config.handler.prefix;
+        prefix = config.handler.prefix;
+
+        if (!message.content.startsWith(prefix)) return;
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const commandInput = args.shift().toLowerCase();
+
+        if (!commandInput.length) return;
+
+        let command =
+            client.collection.prefixcommands.get(commandInput) ||
+            client.collection.prefixcommands.get(
+                client.collection.aliases.get(commandInput)
+            );
+
+        if (command) {
+            try {
+                if (
+                    command.structure?.permissions &&
+                    !message.member.permissions.has(command.structure?.permissions)
+                ) {
+                    await message.reply({
+                        content:
+                            config.messageSettings.notHasPermissionMessage !== undefined &&
+                                config.messageSettings.notHasPermissionMessage !== null &&
+                                config.messageSettings.notHasPermissionMessage !== ""
+                                ? config.messageSettings.notHasPermissionMessage
+                                : "You do not have the permission to use this command.",
+                    });
+
+                    return;
+                }
+
+                if (command.structure?.developers) {
+                    if (!config.users.developers.includes(message.author.id)) {
+                        setTimeout(async () => {
+                            await message.reply({
+                                content:
+                                    config.messageSettings.developerMessage !== undefined &&
+                                        config.messageSettings.developerMessage !== null &&
+                                        config.messageSettings.developerMessage !== ""
+                                        ? config.messageSettings.developerMessage
+                                        : "You are not authorized to use this command",
+                            });
+                        }, 5 * 1000);
+                    }
+
+                    return;
+                }
+
+                if (command.structure?.nsfw && !message.channel.nsfw) {
+                    await message.reply({
+                        content:
+                            config.messageSettings.nsfwMessage !== undefined &&
+                                config.messageSettings.nsfwMessage !== null &&
+                                config.messageSettings.nsfwMessage !== ""
+                                ? config.messageSettings.nsfwMessage
+                                : "The current channel is not a NSFW channel.",
+                    });
+
+                    return;
+                }
+
+                if (command.structure?.cooldown) {
+                    const cooldownFunction = () => {
+                        let data = cooldown.get(message.author.id);
+
+                        data.push(commandInput);
+
+                        cooldown.set(message.author.id, data);
+
+                        setTimeout(() => {
+                            let data = cooldown.get(message.author.id);
+
+                            data = data.filter((v) => v !== commandInput);
+
+                            if (data.length <= 0) {
+                                cooldown.delete(message.author.id);
+                            } else {
+                                cooldown.set(message.author.id, data);
+                            }
+                        }, command.structure?.cooldown);
+                    };
+
+                    if (cooldown.has(message.author.id)) {
+                        let data = cooldown.get(message.author.id);
+
+                        if (data.some((v) => v === commandInput)) {
+                            await message.reply({
+                                content:
+                                    (config.messageSettings.cooldownMessage !== undefined &&
+                                        config.messageSettings.cooldownMessage !== null &&
+                                        config.messageSettings.cooldownMessage !== ""
+                                        ? config.messageSettings.cooldownMessage
+                                        : "Slow down buddy! You're too fast to use this command ({cooldown}s).").replace(/{cooldown}/g, command.structure.cooldown / 1000),
+                            });
+
+                            return;
+                        } else {
+                            cooldownFunction();
+                        }
+                    } else {
+                        cooldown.set(message.author.id, [commandInput]);
+
+                        cooldownFunction();
+                    }
+                }
+
+                command.run(client, message, args);
+            } catch (error) {
+                log(error, "err");
+            }
+        }
+    },
+};
